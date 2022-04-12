b09a0385e25248bde78a0d25ac2fb98094aa3fe0
PhilSoGooood <peelhw@gmail.com>
2022-04-13 00:53:57 +0900

    Chapter06 빈 라이프사이클과 범위

---
+++ sp5-chap06/src/test/java/com/sp5chap06/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp5chap06.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
